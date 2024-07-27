5fcdfad075ebe2e5845d4574aeefb5f5f90c9e44
Debayan92 <71854333+Debayan92@users.noreply.github.com>
2024-07-28 01:40:40 +0530

    added how to swap variables in Q1 and created Q2 to start solving another question

---
+++ src/Q1.java
@@ -0,0 +1,14 @@
+public class Q1 {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 5;
+
+        System.out.println("a = " + a + ", b = " + b);
+
+        int temp = b;
+        b = a;
+        a = temp;
+
+        System.out.println("a = " + a + ", b = " + b);
+    }
+}

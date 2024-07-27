2f0d922de72912f71ce319f07526bffd14cc2c16
Debayan92 <71854333+Debayan92@users.noreply.github.com>
2024-07-28 01:44:59 +0530

    completed Q2

--- src/Q2.java
+++ src/Q2.java
@@ -1,2 +1,9 @@
 public class Q2 {
+    public static void main(String[] args) {
+        double x = 10;
+        double y = 20;
+
+        double z = (x + 10) / (3 * y);
+        System.out.println(z);
+    }
 }

c30b952839e814399712814c8051a81aee3f1058
Debayan92 <71854333+Debayan92@users.noreply.github.com>
2024-07-28 01:55:14 +0530

    complete Q3

---
+++ src/Q3.java
@@ -0,0 +1,16 @@
+public class Q3 {
+    public static void main(String[] args) {
+        int sales = 95_000;
+        double incomeTaxRate = 0.04;
+        double GST = 0.02;
+
+        double incomeTax = sales * incomeTaxRate;
+        double GSTPaid = sales * GST;
+        double totalTax = incomeTax + GSTPaid;
+
+        System.out.println("Total Sales: " + sales);
+        System.out.println("Income Tax Rate: " + incomeTax);
+        System.out.println("GST: " + GSTPaid);
+        System.out.println("Total Tax: " + totalTax);
+    }
+}

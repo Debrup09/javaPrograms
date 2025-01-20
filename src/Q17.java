bd125c5cd3da2c4c7bfb4b94343cc33e2f6fa830
Debrup09 <sahadebrup185@gmail.com>
2025-01-21 03:26:55 +0530

    Added Q16 where you can print square using *

---
+++ src/Q17.java
@@ -0,0 +1,18 @@
+//program to flip the input and print it
+
+import java.util.Scanner;
+
+public class Q17 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number: ");
+        int number = sc.nextInt();
+        int flip = 0;
+
+        while (number != 0) {
+            flip = number % 10;
+            System.out.print(flip);
+            number /= 10;
+        }
+    }
+}

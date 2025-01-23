b2c79ebb26154d88988d1b759864aa9d74577c32
Debrup09 <sahadebrup185@gmail.com>
2025-01-24 00:39:09 +0530

    Added E22 - adding function basics in easy problems list

---
+++ src/easyProblems/E22.java
@@ -0,0 +1,13 @@
+//Function Basics - Printing Hello World using Functions
+
+package easyProblems;
+
+public class E22 {
+
+    public static void printHelloWorld() {
+        System.out.println("Hello World!");
+    }
+    public static void main(String[] args) {
+        printHelloWorld(); //function call
+    }
+}

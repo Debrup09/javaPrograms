993c9c690637a4d253fd5fe90a8e7748f6e7b32d
Debrup09 <sahadebrup185@gmail.com>
2025-01-26 21:36:48 +0530

    EM19 - Solid Rhombus Pattern

---
+++ src/easyModerateProblems/EM19.java
@@ -0,0 +1,27 @@
+package easyModerateProblems;
+
+import java.util.Scanner;
+
+public class EM19 {
+
+    public static void pattern(int n) {
+        for (int i=1; i<=n; i++) {
+            for (int j=1; j<=n+(n-i); j++) {
+                if (j<=n-i) {
+                    System.out.print(" ");
+                } else {
+                    System.out.print("*");
+                }
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter number of lines: ");
+        int n = sc.nextInt();
+
+        pattern(n);
+    }
+}

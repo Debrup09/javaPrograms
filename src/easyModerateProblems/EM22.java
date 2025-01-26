78d5dc01bc381fee1c60e4a755c6c433e74d72da
Debrup09 <sahadebrup185@gmail.com>
2025-01-27 00:11:06 +0530

    EM22 - Number Pyramid

---
+++ src/easyModerateProblems/EM22.java
@@ -0,0 +1,28 @@
+//Number Pyramid
+
+package easyModerateProblems;
+
+import java.util.Scanner;
+
+public class EM22 {
+    public static void pattern(int n) {
+        for (int i=1; i<=n; i++) {
+            for (int j=1; j<=n-i; j++) {
+                System.out.print(" ");
+            }
+
+            for (int j=1; j<=i; j++) {
+                System.out.print(i+" ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter number of lines: ");
+        int n = sc.nextInt();
+
+        pattern(n);
+    }
+}

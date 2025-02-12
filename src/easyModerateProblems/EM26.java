560d90868c68a49a84c837bccac279288b40e7e4
Debrup09 <sahadebrup185@gmail.com>
2025-02-12 17:09:51 +0530

    EM26 - Added User Input Functionality

--- src/easyModerateProblems/EM26.java
+++ src/easyModerateProblems/EM26.java
@@ -2,7 +2,23 @@
 
 package easyModerateProblems;
 
+import java.util.Scanner;
+
 public class EM26 {
+
+    public static int[] arrayInit() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Number of Elements: ");
+        int limit = sc.nextInt();
+        int[] num = new int[limit];
+
+        for (int i=0; i<num.length; i++) {
+            System.out.print("Enter a Number: ");
+            num[i] = sc.nextInt();
+        }
+
+        return num;
+    }
 
     public static void printArray(int[] arr) {
         for (int i=0; i<arr.length; i++) {
@@ -13,7 +29,7 @@ public class EM26 {
     public static void revArray(int[] arr) {
         int first = 0, last = arr.length - 1;
         int temp;
-        while(first!=last) {
+        while(first<last) {
             temp = arr[first];
             arr[first] = arr[last];
             arr[last] = temp;
@@ -25,7 +41,7 @@ public class EM26 {
     }
 
     public static void main(String[] args) {
-        int[] num = {4, 6, 5 ,2 ,8};
+        int[] num = arrayInit();
 
         revArray(num);
     }

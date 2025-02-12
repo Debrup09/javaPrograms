066508b5686c621374318df59ea76f0054e987d7
Debrup09 <sahadebrup185@gmail.com>
2025-02-12 17:07:39 +0530

    EM25 - Added User Input Functionality

--- src/easyModerateProblems/EM25.java
+++ src/easyModerateProblems/EM25.java
@@ -2,7 +2,23 @@
 
 package easyModerateProblems;
 
+import java.util.Scanner;
+
 public class EM25 {
+
+    public static int[] arrayInit() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Number of Elements: ");
+        int limit = sc.nextInt();
+        int[] num = new int[limit];
+
+        for (int i=0; i<num.length; i++) {
+            System.out.print("Enter a Number: ");
+            num[i] = sc.nextInt();
+        }
+
+        return num;
+    }
 
     public static int binarySearch(int[] arr, int n) {
         int start = 0;
@@ -23,8 +39,11 @@ public class EM25 {
     }
 
     public static void main(String[] args) {
-        int[] num = {2, 4, 6, 8, 10, 12, 14, 16, 18, 20};
-        int key = 14;
+        Scanner sc = new Scanner(System.in);
+
+        int[] num = arrayInit();
+        System.out.println("Enter the number you want to find: ");
+        int key = sc.nextInt();
         int result = binarySearch(num, key);
 
         if (result != -1) {

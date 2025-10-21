0cd29715d22860800875e0abe64accf1cdc1e84f
Debrup09 <sahadebrup185@gmail.com>
2025-10-21 12:11:58 +0530

    - changed and optimized the recursive problem

--- src/moderateProblems/sumOfOddNumbersInArrayUsingRecursion.java
+++ src/moderateProblems/sumOfOddNumbersInArrayUsingRecursion.java
@@ -3,21 +3,14 @@ package moderateProblems;
 public class sumOfOddNumbersInArrayUsingRecursion {
     public static int recursion (int[] num, int len) {
         if (len == 0) {
-            if (num[len] % 2 != 0) {
-                return num[len];
-            } else {
-                return 0;
-            }
+            return num[len] % 2 != 0 ? num[len] : 0;
         }
-        if (num[len] % 2 != 0) {
-            return num[len] + recursion(num, --len);
-        } else {
-            return recursion(num, --len);
-        }
+
+        return (num[len] % 2 != 0 ? num[len] : 0) + recursion(num, --len);
     }
 
     public static void main(String[] args) {
-        int[] a = {5, 3, 2};
+        int[] a = {5, 3, 5};
         int sum = recursion(a, a.length-1);
         System.out.println(sum);
     }
